33a6b2dbab05bfa77938e70fd8da57acda021a72
hustrub <hustrubyte@gmail.com>
2023-07-28 13:23:06 -0700

    redo of task

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 0x11. C - printf task
+27-07-2023
+done by Dunu and Nuha
+for printf task
--- utils.c
+++ utils.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated...
- *
- * Return: 1 if c is printable, 0 otherwise
+ * is_printable - Evaluates if a char is printable return 0 if otherwise 
+ * @c: Char to be evaluated to give result if tally to the requirement
+ * done by partners for submission
+ * Return: 1 if c is printable, (0) otherwise
  */
 int is_printable(char c)
 {
@@ -15,33 +15,34 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * hexa_code - Append ascci in hexadecimal code to buffer
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
+ * @ascii_code means  ASSCI CODE.
  * Return: Always 3
+ * done by Nuha
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
+	char map_to_it[] = "0123456789ABCDEF";
 
-	/* The hexa format code is always 2 digits long */
+	/* The hexa format code is always 2 digits long to meet the requirement of ASSCI */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map_to[ascii_code / 16];
-	buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map_to_it[ascii_code / 16];
+	buffer[i] = map_to_it[ascii_code % 16];
 
 	return (3);
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
+ * is_digit - Verifies if char is a digit or word
+ * @c: Char to be evaluated to validate the code 
+ * redo on purpose
  * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
@@ -53,10 +54,10 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
+ * convert_size_number - Casts numb to the specified size
+ * @num: Num to be casted to a specific size
  * @size: Number indicating the type to be casted.
- *
+ * count of number to cast
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -73,7 +74,7 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
+ * similer to above: read more
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
